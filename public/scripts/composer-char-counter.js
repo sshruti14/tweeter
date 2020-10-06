a1a2a686be0c03657efa628391bfc65485bf6856
Shruti Shetty <shetty_14august@yahoo.com>
2020-10-06 21:05:26 +0000

    Creating new tweets section

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // --- our code goes here ---
-  console.log('Load');
+  console.log("Load");
 
-  $(".new-tweet  form textarea").on('keyup',function(){ 
-    let counter = $(this).val().length;  
-   
-    let remainder = (140 - counter);
+  $(".new-tweet  form textarea").on("keyup", function () {
+    let counter = $(this).val().length;
+
+    let remainder = 140 - counter;
 
     $("#counter").val(remainder);
-    $("#counter").css('color', remainder < 0 ? 'red' : 'black');
-
- })
+    $("#counter").css("color", remainder < 0 ? "red" : "black");
+  });
 });
