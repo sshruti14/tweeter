d0f9000a8237e2bf51a84ccab7ae4018cb2c36c2
Shruti Shetty <shetty_14august@yahoo.com>
2020-10-06 18:32:24 +0000

    Implementing the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log('Load');
+
+  $(".new-tweet  form textarea").on('keyup',function(){ 
+    let counter = $(this).val().length;  
+   
+    let remainder = (140 - counter);
+
+    $("#counter").val(remainder);
+    $("#counter").css('color', remainder < 0 ? 'red' : 'black');
+
+ })
+});
