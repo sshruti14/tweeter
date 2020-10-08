a1d0c4e42d683492c39834795ff3ff80f2c02825
Shruti Shetty <shetty_14august@yahoo.com>
2020-10-08 22:20:28 +0000

    Removing unwanted code and cleaning the code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
-  // --- our code goes here ---
   console.log("Load");
 
   $(".new-tweet  form textarea").on("click keypress keyup keydown change", function () {
     $("#errorMsg").hide();
     let counter = $(this).val().length;
-
     let remainder = 140 - counter;
 
     $("#counter").val(remainder);
